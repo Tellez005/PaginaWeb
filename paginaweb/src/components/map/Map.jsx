c21c30fc4a6da478b027509c6c9868128fc88730
Tellez05 <A01640907@tec.mx>
2026-03-10 14:35:21 -0600

    El pepe

--- paginaweb/src/components/map/Map.jsx
+++ paginaweb/src/components/map/Map.jsx
@@ -1,20 +1,36 @@
-import React  from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect, useRef, useState }  from "react";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON} from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
 const Map = () => {
+
+    const [geodata,setGeoData] = useState(null); 
+    const mapRef = useRef(); 
+    const position = [20.73822228680415, -103.4472214186193]; 
+
+    useEffect(() => {
+        fetch("/data/guadalajara_sublocs.geojson")
+        .then((res) =>res.json())
+        .then((data) => setGeoData(data)) 
+        .catch((err) => console.error("Failed to load GeoJSON", err))
+    })
+
     return(
         <>
-        <div>
+        <div style={{height:"600px"}}>
             <h4>Sublocations</h4>     
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer 
+            center={position} 
+            zoom={13} 
+            scrollWheelZoom={true}
+            ref={mapRef}
+            style={{height:"100%", width: "100%"}}
+            
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {geodata && <GeoJSON data={geodata}/> }
             </MapContainer>
         </div>
         </>
